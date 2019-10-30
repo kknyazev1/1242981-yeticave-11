5c4d0bbd866cc9de2f214455f6a1bbab9edbea5e
kknyazev1 <1337kostya@gmail.com>
2019-10-30 15:38:28 +0300

    Массивы

--- index.php
+++ index.php
@@ -2,6 +2,41 @@
 $is_auth = rand(0, 1);
 
 $user_name = ''; // укажите здесь ваше имя
+$massiv1 = [ "Доски и лыжи", "Крепления", 'Ботинки', "Одежда","Инструменты", "Разное"];
+$massiv2 = [
+  [
+     'name' => '2014 Rossignol District Snowboard',
+     'category' => 'Доски и лыжи',
+     'price' => '10999',
+     'URL' => 'img/lot-1.jpg'
+  ],
+  ['name' => 'DC Ply Mens 2016/2017 Snowboard',
+  'category' => 'Доски и лыжи',
+  'price' => '159999',
+  'URL' => 'img/lot-2.jpg'
+  ],
+  ['name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+  'category' => 'Крепления',
+  'price' => '8000',
+  'URL' => 'img/lot-3.jpg'
+  ],
+  ['name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+  'category' => 'Ботинки',
+  'price' => '10999',
+  'URL' => 'img/lot-4.jpg'
+  ],
+  ['name' => 'Куртка для сноуборда DC Mutiny Charocal',
+  'category' => 'Доски и лыжи',
+  'price' => '7500',
+  'URL' => 'img/lot-5.jpg'
+  ],
+  ['name' => 'Маска Oakley Canopy',
+  'category' => 'Доски и лыжи',
+  'price' => '5400',
+  'URL' => 'img/lot-6.jpg'
+  ]
+
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -40,9 +75,11 @@ $user_name = ''; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
+          <?php foreach($massiv1 as $value): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$value;?></a>
             </li>
+          <?php endforeach ?>
         </ul>
     </section>
     <section class="lots">
@@ -50,18 +87,20 @@ $user_name = ''; // укажите здесь ваше имя
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
+
+            <?php foreach($massiv2 as $item): ?>
             <!--заполните этот список из массива с товарами-->
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$item['URL'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$item['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$item['price'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -69,6 +108,7 @@ $user_name = ''; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+        <?php endforeach ?>
         </ul>
     </section>
 </main>
@@ -78,9 +118,11 @@ $user_name = ''; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
+            <?php foreach($massiv1 as $value): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?=$value;?></a>
             </li>
+            <?php endforeach ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
