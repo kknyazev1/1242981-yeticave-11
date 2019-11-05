c0972292acc3b645958414a2f3e0dd2268b79b90
kknyazev1 <1337kostya@gmail.com>
2019-11-05 16:16:45 +0300

    Исправления2(1)

--- templates/main.php
+++ templates/main.php
@@ -4,9 +4,10 @@
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
           <?php foreach($categories as $value): ?>
-            <?=htmlspecialchars($categories);?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$value;?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$value;?>
+                  <?=htmlspecialchars($value);?>
+                </a>
             </li>
           <?php endforeach; ?>
         </ul>
@@ -18,14 +19,15 @@
         <ul class="lots__list">
 
             <?php foreach($ads as $item): ?>
-              <?=htmlspecialchars($ads);?>
             <!--заполните этот список из массива с товарами-->
             <li class="lots__item lot">
                 <div class="lot__image">
                     <img src="<?=$item['URL'];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$item['category'];?></span>
+                    <span class="lot__category"><?=$item['category'];?>
+                      <?=htmlspecialchars($item['category']);?>
+                    </span>
                     <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$item['name'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
